e8138d71e6f4d5214ce9dffb1528186715587b10
happou31 <happo31@gmail.com>
2017-11-30 17:52:25 +0900

    レシーバーの使い方

--- methods.go
+++ methods.go
@@ -13,7 +13,18 @@ func (p Pos) Index(width int) int {
 	return p.y*width + p.x
 }
 
+// ポインタレシーバーの定義
+// ポインタを受け取るので、元のオブジェクトを書き換えられる
+func (p *Pos) Move(x, y int) {
+	p.x = x
+	p.y = y
+}
+
 func main() {
 	p := Pos{10, 10}
-	fmt.Printf("%d\n", p.Index(5)) // レシーバーの使い方
+	fmt.Printf("%d, %d\n", p.x, p.y) // レシーバーの使い方
+
+	p.Move(15, 15)
+	fmt.Printf("%d, %d\n", p.x, p.y) // ポインタレシーバーの使い方
+
 }
