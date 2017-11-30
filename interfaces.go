c4db1441d7778a157cebb1d19464536cb0f425fd
happou31 <happo31@gmail.com>
2017-11-30 18:43:44 +0900

    色々実験 https://go-tour-jp.appspot.com/methods/12

--- interfaces.go
+++ interfaces.go
@@ -15,13 +15,20 @@ type Addable interface {
 }
 
 func (target *A) add(x, y int) {
-	target.x += x
-	target.y += y
+	// レシーバーは実装でnilガードをしておくとよさそう
+	if target != nil {
+		target.x += x
+		target.y += y
+	}
 }
 
 func (target *B) add(a, b int) {
-	target.a += a
-	target.b += b
+	if target != nil {
+		target.a += a
+		target.b += b
+	} else {
+		fmt.Println("<nil>")
+	}
 }
 
 func Add(target Addable, val1, val2 int) {
@@ -31,9 +38,37 @@ func Add(target Addable, val1, val2 int) {
 func main() {
 	a := A{10, 10}
 	b := B{20, 20}
+
+	var nilAdder Addable
+	var nilB *B
+	nilAdder = nilB
+
 	fmt.Printf("%v %v\n", a, b)
-	Add(&a, 5, 2) // 型がinterfaceの場合はポインタを渡す必要がある
+	Add(&a, 5, 2) // レシーバーで要求している型がポインタなのでポインタを渡す必要がある
 	Add(&b, 10, 4)
+	Add(nilAdder, 5, 5)
+
 	fmt.Printf("%v %v\n", a, b)
 
+	PrintTypeInfo(a)
+	PrintTypeInfo(b)
+
+	// createPointer内で作ったオブジェクトのポインタを受け取って、変数の寿命はどうなるか見てみる
+	var x = createPointer()
+	// どうやらスコープを抜けることで寿命が切れてポインタ先が消えることはないらしい(参照が維持されている？)
+	PrintTypeInfo(x)
+	Add(x, 10, 10)
+	PrintTypeInfo(x)
+}
+
+type T interface {
+}
+
+func PrintTypeInfo(t T) {
+	fmt.Printf("%T [%v]\n", t, t)
+}
+
+// スコープ内でオブジェクト作ってポインタを返してみる
+func createPointer() *A {
+	return &A{10, 10}
 }
